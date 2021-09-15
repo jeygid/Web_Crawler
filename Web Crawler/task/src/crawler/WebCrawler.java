0b3e5bcae026e62bf883acbba64fbe6388a85dff
Igor Shestakov <jeygid@yandex.ru>
2021-09-15 11:07:52 +0300

    Commit

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -4,18 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpResponse;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebCrawler extends JFrame {
 
     private JPanel panel;
     private JTextField urlTextField;
     private JButton getText;
-    private JTextArea textArea;
+    private JTable table;
     private JScrollPane scrollTable;
+    private JLabel title;
+    private JLabel titleInfo;
 
     public WebCrawler() {
 
@@ -46,25 +46,43 @@ public class WebCrawler extends JFrame {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
 
-                String source = urlTextField.getText();
-                textArea.setText(HttpRequest.get(source));
+                String sourceUrl = urlTextField.getText();
+                String sourceCode = HttpRequest.get(sourceUrl);
+                //table.setText(sourceCode);
 
-
+                Pattern pattern = Pattern.compile("<title>[\\S ]+</title>");
+                Matcher matcher = pattern.matcher(sourceCode);
+                String title = "";
+                if (matcher.find()) {
+                    title = matcher.group();
+                    title = title.replaceAll("<[/]?title>", "");
+                    titleInfo.setText(title);
+                }
 
             }
         });
 
-        textArea = new JTextArea();
-        textArea.setBounds(20, 50, 740, 600);
-        textArea.setName("HtmlTextArea");
-        textArea.setText("HtmlTextArea");
-        textArea.setEnabled(false);
-        //textArea.setLineWrap(true);
-        textArea.setFont(new Font("Verdana", Font.PLAIN, 12));
-        panel.add(textArea);
+        title = new JLabel();
+        title.setBounds(20, 50, 30, 10);
+        title.setName("Title");
+        title.setText("Title:");
+        panel.add(title);
 
-        scrollTable = new JScrollPane(textArea);
-        scrollTable.setBounds(20, 50, 740, 600);
+        titleInfo = new JLabel();
+        titleInfo.setBounds(60, 50, 200, 10);
+        titleInfo.setName("TitleLabel");
+        panel.add(titleInfo);
+
+        table = new JTable();
+        table.setBounds(20, 70, 740, 580);
+        table.setName("TitlesTable");
+        table.setEnabled(false);
+        //textArea.setLineWrap(true);
+        table.setFont(new Font("Verdana", Font.PLAIN, 12));
+        panel.add(table);
+
+        scrollTable = new JScrollPane(table);
+        scrollTable.setBounds(20, 70, 740, 580);
         scrollTable.setVisible(true);
         scrollTable.setPreferredSize(new Dimension());
         add(scrollTable);
